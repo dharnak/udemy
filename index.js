b4423f2ba4b294ff046400433dce19864e3aa90e
dharnak <thangavelraja36@gmail.com>
2025-05-14 12:12:25 +0530

    upadate:1.1

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.getElementById("udemybus").addEventListener("click",function(){
    window.location.href="login.html";
 })
+// menubar closing function
+document.getElementById("closeX").addEventListener("click",function(){
+   document.getElementById("dropdownmenu").style.display="none";
+})
